1dc6990bdc9d127f85b4070c906688ef27f42cf6
Chris Navarro <ejnavarro06@gmail.com>
2022-08-17 15:42:21 +0800

    modify script

--- hidden.py
+++ hidden.py
@@ -1,1 +1,14 @@
-secret = '{"username":"arista","password":"eve","html5":"-1"}'
+
+platform = input("Enter the Platform to use(e.i. eos,nxos,iosl3,iosl2): ")
+
+if platform == "eos":
+    secret = '{"username":"arista","password":"eve","html5":"-1"}'
+
+elif platform == "nxos":
+    secret = '{"username":"nxos","password":"eve","html5":"-1"}'
+
+elif platform == "iosl3":
+    secret = '{"username":"iosl3","password":"eve","html5":"-1"}'
+
+elif platform == "iosl2":
+    secret = '{"username":"iosl2","password":"eve","html5":"-1"}'
